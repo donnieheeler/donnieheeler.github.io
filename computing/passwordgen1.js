cbe4c12c6d888776698f761809201b07ca1cd5f0
Donnie Heeler <bbryanruizz@gmail.com>
2025-03-12 20:55:52 -0700

    Update passwordgen1.js

--- computing/passwordgen1.js
+++ computing/passwordgen1.js
@@ -4,20 +4,29 @@ async function generatePassword() {
     let words = [];
 
     for (let i = 0; i < 3; i++) {
-        let response = await fetch("https://en.wikipedia.org/wiki/Special:Random");
-        let url = response.url;
-        wikipediaPages.push(url);
+        let iframe = document.createElement("iframe");
+        iframe.style.display = "none"; // Hide the iframe
+        document.body.appendChild(iframe);
 
-        let pageResponse = await fetch(url);
-        let pageText = await pageResponse.text();
-        let wordArray = pageText.replace(/<[^>]*>/g, ' ').split(/\s+/);
-        wordArray = wordArray.filter(word => word.length > 2); // Remove short words
+        let loaded = new Promise((resolve, reject) => {
+            iframe.onload = () => resolve(iframe);
+            iframe.onerror = reject;
+        });
 
+        iframe.src = "https://en.wikipedia.org/wiki/Special:Random";
+        await loaded;
+
+        let textContent = iframe.contentDocument.body.innerText;
+        document.body.removeChild(iframe); // Remove after loading
+
+        let wordArray = textContent.split(/\s+/).filter(word => word.length > 2);
         let randomIndex = Math.floor(Math.random() * wordArray.length);
         words.push(wordArray[randomIndex]);
+
+        wikipediaPages.push(iframe.src);
     }
 
-    let digits = Math.floor(1000 + Math.random() * 9000); // 4 random digits
+    let digits = Math.floor(1000 + Math.random() * 9000);
     let specialChars = "!@#$%^&*";
     let specialChar = specialChars[Math.floor(Math.random() * specialChars.length)];
 
@@ -28,3 +37,4 @@ async function generatePassword() {
         `<p><strong>Wikipedia Pages Used:</strong></p>` +
         wikipediaPages.map(url => `<p><a href='${url}' target='_blank'>${url}</a></p>`).join('');
 }
+
